fd2dc32cd1b73b6a1f4c59f645783a6aef7c0a45
coder-at-411046230487 <ruben@finansdrift.no>
2024-10-16 09:44:35 +0200

    typo

--- contextmenu.js
+++ contextmenu.js
@@ -1,5 +1,5 @@
 /**
- * Dynamic context menues
+ * Dynamic context menus
  * Script to demonstrate how to programmatically create a context menu with three options 
  * and add event listeners to them. This also demonstrates how to programmatically remove the elements and their eventhandlers
  */ 
